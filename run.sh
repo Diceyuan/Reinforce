0631026288d644670ed684754e645572e28f53b6
renqi <rq@janesi.com>
2018-08-08 10:43:47 +0800

    111

--- run.sh
+++ run.sh
@@ -3,15 +3,16 @@
 
 set +x # 抑制输出脚本内容
 
-rootDir=$1
-buildDir=$2
+
+shellDir="/usr/renqi/rein_shell"
+buildDir=$1
 build_type="Release"
 
 if [ ${build_type} == 'Release' ]
 then
     # 参数：keystore路径、keystore密码、keystore别名、keystore别名密码、待加固apk路径filter、加固后apk输出目录、增强服务
-    bash +ex ${rootDir}/reinforce.sh $buildDir
+    bash +ex ${shellDir}/reinforce.sh $buildDir
 else
-    bash +ex ${rootDir}/reinforce.sh $buildDir
+    bash +ex ${shellDir}/reinforce.sh $buildDir
 fi
 
